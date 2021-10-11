12fb5d36eb07c723706e252c3c63feae63fbdc6b
satish venkatakrishnan <svenkatakris@vmware.com>
2021-10-11 12:24:08 +0530

    added storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,19 @@
+import * as sst from "@serverless-stack/resources"
+
+export default class StorageStack extends sst.Stack {
+    table;
+    bucket;
+    constructor(scope,id,props){
+        super(scope,id,props);
+
+        this.table = new sst.Table(this,"Todo",{
+            fields: {
+                userId:sst.TableFieldType.STRING,
+                todoId:sst.TableFieldType.STRING
+            },
+            primaryIndex: {partitionKey:"userId",sortKey:"todoId"},
+        });
+
+        this.bucket = new sst.Bucket(this,"uploads");
+    }
+}
--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,12 @@
-import MyStack from "./MyStack";
+import StorageStack from "./StorageStack";
 
 export default function main(app) {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
     runtime: "nodejs12.x"
   });
-
-  new MyStack(app, "my-stack");
-
+ 
+   new StorageStack(app,"storage");
+  
   // Add more stacks
 }
